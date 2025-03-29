22cc16126d0c1f6f233e3848c4fa559c6d56f3ce
babyboy <bugs.bunny.shah@gmail.com>
2025-03-29 10:35:46 -0400

    DataReplicationClient: start_phase

--- data_applications/data_replication/src/main/java/com/appgallabs/DataReplicationClient.java
+++ data_applications/data_replication/src/main/java/com/appgallabs/DataReplicationClient.java
@@ -6,12 +6,14 @@ import com.appgallabs.dataplatform.client.sdk.api.DataPlatformService;
 import com.appgallabs.dataplatform.util.JsonUtil;
 import com.appgallabs.dataplatform.util.Util;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 public class DataReplicationClient {
 
     public static void main(String[] args) throws Exception{
+        DataReplicationClient dataReplicationClient = new DataReplicationClient();
         DataPlatformService dataPlatformService = DataPlatformService.getInstance();
 
         String apiKey = "ffb2969c-5182-454f-9a0b-f3f2fb0ebf75";
@@ -23,14 +25,6 @@ public class DataReplicationClient {
         System.out.println("*****DATA_SET******");
         JsonUtil.printStdOut(datasetElement);
 
-        String configLocation = "pipe_config/pipe_config.json";
-        String pipeConfigJson = Util.loadResource(configLocation);
-        JsonObject configJson = JsonUtil.validateJson(pipeConfigJson).getAsJsonObject();
-        String pipeId = configJson.get("pipeId").getAsString();
-        String entity = configJson.get("entity").getAsString();
-        System.out.println("*****PIPE_CONFIGURATION******");
-        JsonUtil.printStdOut(configJson);
-
         //configure the DataPipeline Client
         Configuration configuration = new Configuration().
                 ingestionHostUrl("http://localhost:8080/").
@@ -39,12 +33,65 @@ public class DataReplicationClient {
                 streamSizeInObjects(0);
         dataPlatformService.configure(configuration);
 
-        //register pipe
-        dataPlatformService.registerPipe(configJson);
-        System.out.println("*****PIPE_REGISTRATION_SUCCESS******");
+        JsonArray replicationPipeline = dataReplicationClient.
+                discoverReplicationPipeLine();
 
-        //send source data through the pipeline
-        dataPlatformService.sendData(pipeId, entity,datasetElement.toString());
+        //register pipes
+        dataReplicationClient.registerReplicationPipeLine(dataPlatformService,
+                replicationPipeline
+        );
+
+        //send source data through the replication pipeline (collection of pipes)
+        dataReplicationClient.sendData(dataPlatformService,
+                replicationPipeline,
+                datasetElement
+        );
+    }
+
+    private JsonArray discoverReplicationPipeLine(){
+        try {
+            JsonArray replicationPipes = new JsonArray();
+
+            String configLocation = "pipe_config/pipe_config.json";
+            String pipeConfigJson = Util.loadResource(configLocation);
+            JsonArray pipesJsonArray = JsonUtil.validateJson(pipeConfigJson).getAsJsonArray();
+            for(int i=0; i<pipesJsonArray.size(); i++) {
+                JsonObject configJson = pipesJsonArray.get(i).getAsJsonObject();
+                System.out.println("*****PIPE_CONFIGURATION******");
+                JsonUtil.printStdOut(configJson);
+                replicationPipes.add(configJson);
+            }
+
+            return replicationPipes;
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void registerReplicationPipeLine(DataPlatformService dataPlatformService,
+                                             JsonArray pipesJsonArray){
+        try {
+            for (int i = 0; i < pipesJsonArray.size(); i++) {
+                JsonObject configJson = pipesJsonArray.get(i).getAsJsonObject();
+                dataPlatformService.registerPipe(configJson);
+                System.out.println("*****PIPE_REGISTRATION_SUCCESS******");
+            }
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void sendData(DataPlatformService dataPlatformService,
+                          JsonArray pipesJsonArray,
+                          JsonElement datasetElement
+    ){
+        //TODO: make it concurrent and in batches
+        for(int i=0; i<pipesJsonArray.size();i++) {
+            JsonObject configJson = pipesJsonArray.get(i).getAsJsonObject();
+            String pipeId = configJson.get("pipeId").getAsString();
+            String entity = configJson.get("entity").getAsString();
+            dataPlatformService.sendData(pipeId, entity, datasetElement.toString());
+        }
         System.out.println("*****DATA_INGESTION_SUCCESS******");
     }
 }
